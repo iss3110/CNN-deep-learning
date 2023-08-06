7091df3227bcbab2bfab410f8b68244db1913922
isslao <sslaouane@gmail.com>
2023-08-07 00:11:59 +0200

    Update modele.py

--- modele.py
+++ modele.py
@@ -1,5 +1,6 @@
 # principe de filtre
-# Avant de nous attaquer au deep learning il est important de comprendre ce qu'on fait en terme de traitement d'image. On va notamment s'intéresser dans cette première partie à la notion de noyau appliqué sur une image.
+# Avant de nous attaquer au deep learning il est important de comprendre ce qu'on fait en terme de traitement d'image.
+# On va notamment s'intéresser dans cette première partie à la notion de noyau appliqué sur une image.
 # Une image est un condensé de données, que l'on appel les pixels.
 import cv2
 import numpy as np
@@ -18,5 +19,6 @@ image_array_ticket = np.asarray(bytearray(resp.read()), dtype="uint8")
 print(f'Shape of the image {image_array_ticket.shape}')
 image_ticket = cv2.imdecode(image_array_ticket, cv2.IMREAD_COLOR)
 plt.axis('off')
-plt.imshow(cv2.cvtColor(image_ticket, cv2.COLOR_BGR2RGB)) #opencv if BGR color, matplotlib usr RGB so we need to switch otherwise the ticket will be blue ... O:)
+plt.imshow(cv2.cvtColor(image_ticket, cv2.COLOR_BGR2RGB)) #opencv if BGR color, matplotlib usr RGB so we need to switch otherwise
+# the ticket will be blue ... O:)
 plt.show()
