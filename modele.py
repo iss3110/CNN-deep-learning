822cd57ed86aca59da1ab52027c56c87f20e719b
isslao <sslaouane@gmail.com>
2023-08-08 16:57:30 +0200

    Update modele.py

--- modele.py
+++ modele.py
@@ -1,24 +1,19 @@
-# principe de filtre
-# Avant de nous attaquer au deep learning il est important de comprendre ce qu'on fait en terme de traitement d'image.
-# On va notamment s'intéresser dans cette première partie à la notion de noyau appliqué sur une image.
-# Une image est un condensé de données, que l'on appel les pixels.
-import cv2
-import numpy as np
-import requests
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import sys
-import datetime
-from tensorflow import keras
-from tensorflow.keras.models import Model
-import tensorflow as tf
+import os
+from PIL import Image
 
-ticket = r'?raw=true'
-resp = requests.get(ticket, stream=True).raw
-image_array_ticket = np.asarray(bytearray(resp.read()), dtype="uint8")
-print(f'Shape of the image {image_array_ticket.shape}')
-image_ticket = cv2.imdecode(image_array_ticket, cv2.IMREAD_COLOR)
-plt.axis('off')
-plt.imshow(cv2.cvtColor(image_ticket, cv2.COLOR_BGR2RGB)) #opencv if BGR color, matplotlib usr RGB so we need to switch otherwise
-# the ticket will be blue ... O:)
-plt.show()
+# Chemin vers le dossier contenant vos images
+image_folder = "chemin/vers/le/dossier/contenant/vos/images"
+
+# Chemin vers le dossier où vous voulez enregistrer les images prétraitées
+output_folder = "chemin/vers/le/dossier/de/sortie"
+
+# Obtenir la liste des fichiers JPG dans le dossier
+image_files = filter(lambda file: file.endswith(".jpg"), os.listdir(image_folder))
+
+# Prétraiter chaque image en utilisant PIL et redimensionner à 224x224
+resize_and_save = lambda filename: Image.open(os.path.join(image_folder, filename)).resize((224, 224)).save(
+    os.path.join(output_folder, filename)
+)
+
+# Utiliser la fonction map pour appliquer la transformation à chaque fichier
+list(map(resize_and_save, image_files))
